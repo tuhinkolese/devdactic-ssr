91151623ab55a199fbd999169cf428671e2faffc
Simon Grimm <saimon@devdactic.com>
2020-06-22 09:59:18 +0200

    Change SEO.

--- src/app/pages/second/second.page.ts
+++ src/app/pages/second/second.page.ts
@@ -13,11 +13,20 @@ export class SecondPage implements OnInit {
   ngOnInit() {
     this.titleService.setTitle('Devdactic SSR');
     this.metaService.updateTag({ name: 'description', content: 'The Devdactic SSR Page' });
-        // Twitter
+    // Twitter
     this.metaService.updateTag({ property: 'twitter:card', content: 'summary_large_image' });
     this.metaService.updateTag({ property: 'twitter:title', content: 'NEW ARTICLE OUT NOW' });
     this.metaService.updateTag({ property: 'twitter:description', content: 'Check out this cool article' });
     this.metaService.updateTag({ property: 'twitter:image', content: 'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1' });
+
+    // Facebook
+    this.metaService.updateTag({ property: 'og:url', content: '/second' });
+    this.metaService.updateTag({ property: 'og:type', content: 'website' });
+    this.metaService.updateTag({ property: 'og:description', content: 'My Ionic SSR Page' });
+    this.metaService.updateTag({ property: 'og:title', content: 'My SSR Title!' });
+    this.metaService.updateTag({ property: 'og:image', content: 'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1' });
+
+
   }
 
 }
