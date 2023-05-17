5793662ea8ff532504ca101bef29d9888c359b02
tuhin.kole <tuhin.kole@stockedge.com>
2023-05-17 16:43:18 +0530

    add seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private metaService: Meta,
+    private titleService: Title
   ) {
     this.initializeApp();
   }
@@ -24,4 +27,34 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+
+
+  getSEOData() {
+  
+            // Twitter
+            this.metaService.updateTag({ property: 'twitter:card', content: 'summary_large_image' });
+            this.metaService.updateTag({ property: 'twitter:title', content: 'NEW ARTICLE OUT NOW' });
+            this.metaService.updateTag({ property: 'twitter:url', content: "https://tense-pig-garters.cyclic.app/" });
+            this.metaService.updateTag({ property: 'twitter:description', content: 'Check out this cool article' });
+            this.metaService.updateTag({ property: 'twitter:image', content: 'https://web.stockedge.com/assets/img/light/icon.png' });
+        
+            // Facebook
+            this.metaService.updateTag({ property: 'og:url', content: '/second' });
+            this.metaService.updateTag({ property: 'og:type', content: 'website' });
+            this.metaService.updateTag({ property: 'og:description', content: 'My Ionic SSR Page' });
+            this.metaService.updateTag({ property: 'og:title', content: 'My SSR Title!' });
+            this.metaService.updateTag({ property: 'og:image', content: 'https://web.stockedge.com/assets/img/light/icon.png' });
+
+            //SEO
+            this.metaService.updateTag({ name: "title", content: 'StockEdge Title' });
+            this.metaService.updateTag({ name: "description", content: "Stockedge Desc" });
+            this.metaService.updateTag({ name: "keywords", content: 'stockedge' });
+            this.titleService.setTitle('Stockedge')
+
+
+            
+      }
+  
+
 }
--- src/app/pages/second/second.page.ts
+++ src/app/pages/second/second.page.ts
@@ -8,23 +8,52 @@ import { Title, Meta } from '@angular/platform-browser';
 })
 export class SecondPage implements OnInit {
 
-  constructor(private titleService: Title, private metaService: Meta) { }
+  constructor(private titleService: Title, private metaService: Meta) {
+    this.addDefaultTags()
+   }
 
   ngOnInit() {
+
     this.titleService.setTitle('Devdactic SSR');
-    this.metaService.updateTag({ name: 'description', content: 'The Devdactic SSR Page' });
+    this.metaService.updateTag({ name: 'description', content: 'The SSR Page' });
     // Twitter
     this.metaService.updateTag({ property: 'twitter:card', content: 'summary_large_image' });
     this.metaService.updateTag({ property: 'twitter:title', content: 'NEW ARTICLE OUT NOW' });
+    this.metaService.updateTag({ property: 'twitter:url', content: "https://tense-pig-garters.cyclic.app/" });
     this.metaService.updateTag({ property: 'twitter:description', content: 'Check out this cool article' });
-    this.metaService.updateTag({ property: 'twitter:image', content: 'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1' });
+    this.metaService.updateTag({ property: 'twitter:image', content: 'https://web.stockedge.com/assets/img/light/icon.png' });
 
     // Facebook
     this.metaService.updateTag({ property: 'og:url', content: '/second' });
     this.metaService.updateTag({ property: 'og:type', content: 'website' });
     this.metaService.updateTag({ property: 'og:description', content: 'My Ionic SSR Page' });
     this.metaService.updateTag({ property: 'og:title', content: 'My SSR Title!' });
-    this.metaService.updateTag({ property: 'og:image', content: 'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1' });
+    this.metaService.updateTag({ property: 'og:image', content: 'https://web.stockedge.com/assets/img/light/icon.png' });
+
+
+  }
+
+
+  addDefaultTags() {
+    this.metaService.addTags([
+      // Twitter
+      { name: "twitter:card", content: "summary" },
+      { name: "twitter:site", content: "@mystockedge" },
+      { name: "twitter:title", content:'The SSR Page' },
+      { name: "twitter:description", content: 'The SSR Page' },
+      { name: "twitter:url", content: 'https://web.stockedge.com/assets/img/light/icon.png' }, // URL_SE
+      { name: "twitter:creator", content: "@mystockedge" },
+
+      { property: "fb:app_id", content: "123456" }, // 2) taken from FB page of mystockedge 1) app id taken from config.xml
+      { property: "og:title", content: "this.defaultSeoTitle" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: 'https://web.stockedge.com/assets/img/light/icon.png' }, // URL_SE
+      { property: "og:description", content: "this.defaultSeoDesc" },
+      //SEO 
+      { name: "description", content: "this.defaultSeoDesc" },
+      { name: "title", content: "this.defaultSeoTitle" },
+      { name: "keywords", content: "stockedge" }
+    ]);
 
 
   }
